228361f04d22b1047e12fa7faabc0dd572b4cc27
EasternPen9uin <yck723@naver.com>
2024-05-10 21:27:48 +0900

    editted getLawDetail api

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, jwt_required
 from dotenv import load_dotenv; load_dotenv()
 from utils import *
@@ -8,22 +8,27 @@ application = Flask(import_name = __name__)
 
 # env파일에서 OC 획득
 OC = os.environ.get("OC")
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+
 # 제대로 설정되지 않았을 경우 경고용 메시지 출력
 if OC == "test" or OC == None:
     print(".env파일 내 OC가 제대로 설정되지 않았습니다!\nThe OC in the .env file is not properly set up!")
+if JWT_SECRET_KEY is None:
+    print(".env파일 내 JWT_SECRET_KEY가 제대로 설정되지 않았습니다!\nJWT_SECRET_KEY in the .env file is not properly set up!")
 
 # 토큰 생성에 사용될 Secret Key를 flask 환경 변수에 등록
 application.config.update(
 	DEBUG = False,
-	JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+	JWT_SECRET_KEY = JWT_SECRET_KEY
 )
 
 # JWT 확장 모듈을 flask 어플리케이션에 등록
 jwt = JWTManager(application)
 
 # (디버그) 주석 해제시 jwt 인증을 해제함
-#jwt_required = empty_decorator
+jwt_required = empty_decorator
 
+# API 인증 확인용
 @application.route("/")
 @jwt_required()
 def test_test():
@@ -43,9 +48,23 @@ def getLawDetail():
     """
     법률 내용 확인
     """
+    # 쿼리에서 idx값을 가져옴 (ResponseTooLarge방지용)
+    idx = 1
+    if request.args.get('idx') is not None:
+        idx = int(request.args.get('idx'))
+    
     # XML형식으로 반환할 경우 ResponseTooLarge오류가 발생할 수 있음. 
     # 따라서 형식 제거 후 반환
-    return parseLegalXML(apiCommonPart(request, OC, "law", "lawService"))
+    content = parseLegalXML(apiCommonPart(request, OC, "law", "lawService"))
+    CONTENT_MAX_LENGTH = 50000
+    contentList = [content[i:i+CONTENT_MAX_LENGTH] for i in range(0, len(content), CONTENT_MAX_LENGTH)]
+    
+    totalpage = len(content)//CONTENT_MAX_LENGTH + 1
+    return jsonify({
+        "totalidx":totalpage,
+        "nowidx":idx,
+        "content":contentList[idx-1]
+    })
 
 @application.route("/searchPrecedent")
 @application.route("/searchPrecedentByCaseNumber") #사건번호로 검색
