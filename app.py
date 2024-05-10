97927a16cd7a5c5cae188c3b6a0db7a74f47b965
EasternPen9uin <yck723@naver.com>
2024-05-10 17:19:39 +0900

    add api code for debugging

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, request
+from flask_jwt_extended import JWTManager, jwt_required
+from dotenv import load_dotenv; load_dotenv()
+from utils import *
+import os
+
+application = Flask(import_name = __name__)
+
+# env파일에서 OC 획득
+OC = os.environ.get("OC")
+# 제대로 설정되지 않았을 경우 경고용 메시지 출력
+if OC == "test" or OC == None:
+    print(".env파일 내 OC가 제대로 설정되지 않았습니다!\nThe OC in the .env file is not properly set up!")
+
+# 토큰 생성에 사용될 Secret Key를 flask 환경 변수에 등록
+application.config.update(
+	DEBUG = True,
+	JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+)
+
+# JWT 확장 모듈을 flask 어플리케이션에 등록
+jwt = JWTManager(application)
+
+@application.route("/")
+@jwt_required()
+def test_test():
+    return "<h1>Flask is running!</h1>"
+
+@application.route("/searchLaws")
+@jwt_required()
+def searchLaws():
+    """
+    법령 목록 검색
+    """
+    return apiCommonPart(request, OC, "law", "lawSearch")
+
+@application.route("/getLawDetail")
+@jwt_required()
+def getLawDetail():
+    """
+    법률 내용 확인
+    """
+    # XML형식으로 반환할 경우 ResponseTooLarge오류가 발생할 수 있음. 
+    # 따라서 형식 제거 후 반환
+    return parseLegalXML(apiCommonPart(request, OC, "law", "lawService"))
+
+@application.route("/searchPrecedent")
+@application.route("/searchPrecedentByCaseNumber") #사건번호로 검색
+@jwt_required()
+def searchPrecedent():
+    """
+    판례 목록 검색
+    """
+    return apiCommonPart(request, OC, "prec", "lawSearch")
+
+@application.route("/getPrecedentDetail")
+@jwt_required()
+def getPrecedentDetail():
+    """
+    판례 내용 확인
+    """
+    return apiCommonPart(request, OC, "prec", "lawService")
+
+@application.route("/getLegalTerm")
+#@jwt_required()
+def getLegalTerm():
+    """
+    법령용어 확인
+    """
+    return apiCommonPart(request, OC, "lstrm", "lawService")
+
+if __name__ == '__main__':
+	application.run(host = '0.0.0.0', port = 5000, debug = True)
---
+++ utils.py
@@ -0,0 +1,70 @@
+def apiCommonPart(request, oc, target, do):
+    # 전체 쿼리 스트링을 가져옴
+    full_query_string = request.query_string.decode('utf-8')
+    # 고정값
+    CONSTANT_STRING :str = f"target={target}&OC={oc}&type=XML"
+    
+    # 입력받은 쿼리를 사용하여 API 요청 보내고 응답 받기
+    url = f"https://www.law.go.kr/DRF/{do}.do?{CONSTANT_STRING}&{full_query_string}"
+    response = sendGetRequest(url)
+    # 응답 반환
+    return response
+
+def sendGetRequest(url):
+    """
+        GET 요청을 보내고 응답을 (주의)문자열로 받는 함수
+    """
+    import urllib.request
+    with urllib.request.urlopen(url) as response:
+        # 응답의 바디를 utf-8로 디코딩하여 출력합니다.
+        response_body = response.read().decode('utf-8')
+        return response_body
+
+def parseLegalXML(xml_string):
+    """
+        법 조문(XML)을 평문으로 바꾸는 함수
+    """
+    import xmltodict
+    import json
+    import io
+    import re
+    
+    # XML을 OrderedDict로 파싱
+    data_dict = xmltodict.parse(xml_string)
+    data_dict = data_dict['법령']
+
+    # StringBuffer역할
+    sio = io.StringIO()
+    def swrite(i=""):
+        sio.write(i)
+        sio.write('\n')
+    
+    # 법령명 입력
+    swrite(data_dict["기본정보"]["법령명_한글"])
+    # 시행 일자 입력
+    swrite(f'시행일자 : {data_dict["기본정보"]["시행일자"]}')
+    # 공포번호 입력
+    swrite(f'공포번호 : 법률 제{data_dict["기본정보"]["공포번호"]}호')
+    # 공포일자 입력
+    swrite(f'공포일자 : {data_dict["기본정보"]["공포일자"]}')
+    # 소관부처 입력
+    swrite(f'소관부처 : {data_dict["기본정보"]["연락부서"]["부서단위"]["소관부처명"]}({data_dict["기본정보"]["연락부서"]["부서단위"]["부서명"]}), {data_dict["기본정보"]["전화번호"]}')
+    swrite()
+    # 내용 입력
+    swrite("내용 : ")
+    for article in data_dict["조문"]["조문단위"]:
+        swrite(article["조문내용"])
+        if "항" in article:
+            for content in json.dumps(article["항"], ensure_ascii=False, indent = 0).split('\n',):
+                if "내용" in content:
+                    swrite(content[content.find(": ")+3:].rstrip('",').replace('\\"','"'))
+    swrite()
+    # 부칙 입력
+    for article in data_dict["부칙"]["부칙단위"]:
+        string = re.sub(r'\n+', '\n', article["부칙내용"])
+        for content in string.split('\n'):
+            swrite(content.strip())
+
+    ret = sio.getvalue()
+    sio.close()
+    return ret
