26a25be9b9aef420a2166a1c4858125645f5ca73
sae0chi <yck723@naver.com>
2024-05-14 08:56:30 +0900

    removed prev code

--- utils.py
+++ utils.py
@@ -66,62 +66,6 @@ def parseLegalXML(xml_string):
 
     return ret_dict, ret_list
 
-    '''
-    import xmltodict
-    import json
-    import io
-    import re
-    
-    # XML을 OrderedDict로 파싱
-    data_dict = xmltodict.parse(xml_string)
-    data_dict = data_dict['법령']
-
-    # StringBuffer역할
-    sio = io.StringIO()
-    def swrite(i=""):
-        sio.write(i)
-        sio.write('\n')
-    
-    # 법령명 입력
-    swrite(data_dict["기본정보"]["법령명_한글"])
-    # 시행 일자 입력
-    try: swrite(f'시행일자 : {data_dict["기본정보"]["시행일자"]}')
-    except: pass
-    # 공포번호 입력
-    try: swrite(f'공포번호 : 법률 제{data_dict["기본정보"]["공포번호"]}호')
-    except: pass
-    # 공포일자 입력
-    try: swrite(f'공포일자 : {data_dict["기본정보"]["공포일자"]}')
-    except: pass
-    # 소관부처 입력
-    try: swrite(f'소관부처 : {data_dict["기본정보"]["연락부서"]["부서단위"]["소관부처명"]}({data_dict["기본정보"]["연락부서"]["부서단위"]["부서명"]}), {data_dict["기본정보"]["전화번호"]}')
-    except: pass
-
-    swrite()
-    # 내용 입력
-    swrite("내용 : ")
-
-    try:
-        for article in data_dict["조문"]["조문단위"]:
-            swrite(article["조문내용"])
-            if "항" in article:
-                for content in json.dumps(article["항"], ensure_ascii=False, indent = 0).split('\n',):
-                    if "내용" in content:
-                        swrite(content[content.find(": ")+3:].rstrip('",').replace('\\"','"'))
-            swrite()
-    except:pass
-    swrite()
-    
-    #부칙 입력
-    for article in data_dict["부칙"]["부칙단위"]:
-        string = re.sub(r'\n+', '\n', article["부칙내용"])
-        for content in string.split('\n'):
-            swrite(content.strip())
-
-    ret = sio.getvalue()
-    sio.close()
-    return ret'''
-
 def empty_decorator():
     """
         디버그용 빈 데코레이터
