dff1b9ac9a81a8386fb03bb6dc36f3e4797abbe0
sae0chi <yck723@naver.com>
2024-05-14 09:12:09 +0900

    added jwt secret issuance logic

--- get_api_key.py
+++ get_api_key.py
@@ -1,11 +1,15 @@
 from dotenv import load_dotenv; load_dotenv()
+from utils import makeNewJWTSecret
 import os
 import jwt
 
 JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 
-if JWT_SECRET_KEY is not None:
-    print("Below is API Key:")
-    print(jwt.encode({}, JWT_SECRET_KEY, algorithm='HS256'))
-else:
-    print("JWT_SECRET_KEY in the .env file is not properly set up!")
+if JWT_SECRET_KEY is None:
+    makeNewJWTSecret()
+    load_dotenv()
+    load_dotenv()
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+
+print("Below is API Key:")
+print(jwt.encode({}, JWT_SECRET_KEY, algorithm='HS256'))
